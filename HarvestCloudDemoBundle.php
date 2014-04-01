9aa275d2da63e5e1dbb81f94689ebb98a6bd451d
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2014-03-31 21:05:05 -0400

    Adding HarvestCloudDemoBundle

---
+++ HarvestCloudDemoBundle.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Harvest Cloud package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HarvestCloud\DemoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * HarvestCloudDemoBundle
+ *
+ * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+ * @since  2014-03-30
+ */
+class HarvestCloudDemoBundle extends Bundle
+{
+}
